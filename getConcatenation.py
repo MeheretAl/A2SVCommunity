afa50986587dcd2c8523cc1c6f642069682de0d6
MeheretAl <meheretxo4@gmail.com>
2024-07-27 23:32:23 +0300

    removed clutter from listComprehension and added getConcatenation

---
+++ getConcatenation.py
@@ -0,0 +1,8 @@
+def getConcatenation( nums: list[int]) -> list[int]:
+        size = len(nums)
+        ans = [0]*(2*size)
+        for i in range(size):
+            ans[i] = nums[i]
+            ans[i+size] = nums[i]
+        
+        return ans
